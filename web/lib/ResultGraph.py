a86b6e7bb702db914328f43b897119e464fced50
sfbrunner <sibrunne@gmail.com>
2017-01-16 21:08:48 +0000

    This library class handles the construction and export of NetworkX-based networks.

--- web/lib/ResultGraph.py
+++ web/lib/ResultGraph.py
@@ -11,11 +11,22 @@ class ResultGraph():
     def add_publication(self, pmid, cite_lst):
         for citation in cite_lst:
             self.G.add_edge(pmid, citation)
+            
+        for n in self.G:
+            # Add 'name' tag to each node
+            self.G.node[n]['name'] = str(n)
+            
+            # Add group tag
+            if self.G.node[n]['name'] == str(pmid):
+                self.G.node[n]['group'] = 'Searched'
+            elif str(self.G.node[n]['name']) in cite_lst:
+                self.G.node[n]['group'] = 'Cited'
     
     def populate_from_cite_dict(self, cite_dict):
         for pmid in cite_dict:
             self.add_publication(pmid, cite_dict[pmid])
 
     def get_json(self):
+        
         d = json_graph.node_link_data(self.G)
         return jsonify(d) 
