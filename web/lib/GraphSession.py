cf0724ee9be53d0d5e60ad4e844cbb6dc4ab1734
sfbrunner <sibrunne@gmail.com>
2017-01-16 21:07:17 +0000

    This library class handles the construction and export of NetworkX-based networks.

--- web/lib/GraphSession.py
+++ web/lib/GraphSession.py
@@ -12,7 +12,7 @@ class GraphSession():
     def load_citations(self):
         cite_dict = {}
         for pmid in self.pmid_lst:
-            cite_dict[pmid] = self.eutils.get_cited_PMID(pmid)
+            cite_dict[pmid] = self.eutils.get_cited_PMID_elink(pmid)
 
         self.cite_dict = cite_dict
              
