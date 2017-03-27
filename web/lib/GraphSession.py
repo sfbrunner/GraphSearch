881abd444f3bd00e04c950d573443a01d1340171
sfbrunner <sibrunne@gmail.com>
2017-03-27 09:05:59 +0000

    Changed lookup behaviour of publications to lookup PMID or PMC ID, depending on input.

--- web/lib/GraphSession.py
+++ web/lib/GraphSession.py
@@ -7,12 +7,16 @@ class GraphSession():
         self.eutils = ConnectEutils()
 
     def parse_input(self):
-        self.pmid_lst = self.user_input.split(',')
+        pmid_lst_raw = self.user_input.split(',')
+	self.pmid_lst=[]
+	for pmid in pmid_lst_raw:
+	    pmid = pmid.strip()
+            self.pmid_lst.append(pmid)
 
     def load_citations(self):
         cite_dict = {}
         for pmid in self.pmid_lst:
-            cite_dict[pmid] = self.eutils.get_cited_PMID_elink(pmid)
+            cite_dict[pmid] = self.eutils.get_cited_pub(pmid)
 
         self.cite_dict = cite_dict
              
