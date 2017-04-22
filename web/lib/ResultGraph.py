319555b439f0619b6b4410a7992056b729d3d829
rmishr <ravi.mishra@gmx.net>
2017-04-22 15:34:43 +0000

    Changes to d3 force graphs and ResultGraph to reflect commonly cited papers

--- web/lib/ResultGraph.py
+++ web/lib/ResultGraph.py
@@ -8,10 +8,15 @@ class ResultGraph():
     def __init__(self):
         # Initialise NetworkX graph
         self.G = nx.Graph()
+        self.citationWeight = {}
 
     def add_publication(self, pmid, cite_lst):
         for citation in cite_lst:
             self.G.add_edge(pmid, citation)
+            if str(pmid) in self.citationWeight.keys():
+                self.citationWeight[str(pmid)] += 1
+            else:
+                self.citationWeight[str(pmid)] = 1
             
         for n in self.G:
             # Add 'name' tag to each node
@@ -26,11 +31,17 @@ class ResultGraph():
     def populate_from_cite_dict(self, cite_dict):
         for pmid in cite_dict:
             self.add_publication(pmid, cite_dict[pmid])
+			
+    def _addWeightsToGraph(self):
+        for citation in self.citationWeight:
+            self.G.node[citation]['weight'] = self.citationWeight[citation] 
 
-    def get_json(self):     
+    def get_json(self):
+        self._addWeightsToGraph()
         d = json_graph.node_link_data(self.G)
         #return json.dumps(d)
         return jsonify(d) 
 
     def get_graph(self):
+        self._addWeightsToGraph()
         return json_graph.node_link_data(self.G)
