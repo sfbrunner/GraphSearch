3d60c080540d15116f62db31c8b672ca8335c823
sfbrunner <sibrunne@gmail.com>
2017-11-20 18:01:47 +0000

    Updated logger to be initialised once per session.
    Logger object is now stored in class variable. New instances of
    the logger do not create additional log handles but access
    the pre-stored object handle.

--- web/utils/logger.py
+++ web/utils/logger.py
@@ -1,12 +1,31 @@
 import logging
 
-class LogHandler():
+def get_logger(log_name = '', fpath = None):
     '''
-    Encapsulates behaviour of creatig logging.Logger handles
+    Either returns an already existing log handle from LogHandler class attribute,
+    or generates a new Log Handle.
     '''
     
+    # Retrieve class attribute from LogHandler
+    lh = LogHandler().lh
+    
+    # If lh is not set, generate new log handler
+    if not lh:
+        lh = LogHandler.get_logger(log_name, fpath)
+        if not fpath:
+            lh.warning('No file path specified to store log. Any logging will only be written to stdout.')
+        else:
+            lh.info('Generated new logger. Writing to file: {f}'.format(f=str(fpath)))
+    return lh
+
+class LogHandler():
+    '''
+    Encapsulates behaviour of creating logging.Logger handles
+    '''
+    lh = None
+    
     @classmethod
-    def get_logger(self, log_name, fpath):
+    def get_logger(self, log_name, fpath=None, debug=False):
         '''
         Returns a logger handle
         
@@ -15,25 +34,36 @@ class LogHandler():
         fpath: path to log file
         '''
         
-        # Initialise logger
-        logger = logging.getLogger(log_name)
-        logger.setLevel(logging.DEBUG)
+        # If lh is not set, generate new log handler
+        if not LogHandler.lh:
+            #print "not lh"
+            # Initialise logger
+            logger = logging.getLogger(log_name)
+            logger.setLevel(logging.DEBUG)
+
+            # Create file handler which logs all messages, including debug level
+            if fpath:
+                fh = logging.FileHandler(fpath)
+                fh.setLevel(logging.DEBUG)
+
+            # Create console handler that only logs at the info level
+            ch = logging.StreamHandler()
+            if debug:
+                ch.setLevel(logging.DEBUG)
+            else:
+                ch.setLevel(logging.INFO)
+                
+            # Create formatter and add it to the handlers
+            formatter = logging.Formatter(fmt='%(asctime)s [%(levelname)s]: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+            if fpath:
+                fh.setFormatter(formatter)
+            ch.setFormatter(formatter)
+
+            # Add the handlers to the logger
+            if fpath:
+                logger.addHandler(fh)
+            logger.addHandler(ch)
+            
+            LogHandler.lh = logger
         
-        # Create file handler which logs all messages, including debug level
-        fh = logging.FileHandler(fpath)
-        fh.setLevel(logging.DEBUG)
-        
-        # Create console handler that only logs at the info level
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.INFO)
-        
-        # Create formatter and add it to the handlers
-        formatter = logging.Formatter(fmt='%(asctime)s [%(levelname)s]: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-        fh.setFormatter(formatter)
-        ch.setFormatter(formatter)
-        
-        # Add the handlers to the logger
-        logger.addHandler(fh)
-        logger.addHandler(ch)
-        
-        return logger
+        return LogHandler.lh
