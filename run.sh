602316f77e4260a06e3add1e7891810150617656
rmishr <ravi.mishra@gmx.net>
2017-11-21 21:14:57 +0100

    Added some test data and shell script to start services.

---
+++ run.sh
@@ -0,0 +1,21 @@
+
+# we need an instance of redis server
+redis-server &
+
+# we need the backend to run
+python backend/restapi.py &
+
+# we need celery to run -> change arguments when multi threading possible
+celery -A worker worker --loglevel=debug -P solo &
+
+# we need an instance of mongo db running
+mongod &
+
+# we need our frontend to run
+python web/httpserver.py && fg
+
+# for editing jsx files we need gulp
+# web/gulp
+
+# test the backend:
+# http PUT http://localhost:8080 search_string="Stratton Cancer Breast Women"
