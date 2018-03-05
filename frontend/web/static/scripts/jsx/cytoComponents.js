83d1af007ada305821cc3b990b7e9449128a7453
sfbrunner <sibrunne@gmail.com>
2018-03-05 23:40:07 +0100

    Updated layout to 'cose'

--- frontend/web/static/scripts/jsx/cytoComponents.js
+++ frontend/web/static/scripts/jsx/cytoComponents.js
@@ -179,7 +179,7 @@ var cytoCoseBilkentLayout = {
     // Whether to include labels in node dimensions. Useful for avoiding label overlap
     nodeDimensionsIncludeLabels: false,
     // number of ticks per frame; higher is faster but more jerky
-    refresh: 30,
+    refresh: 1,
     // Whether to fit the network view after when done
     fit: true,
     // Padding on fit
@@ -193,15 +193,15 @@ var cytoCoseBilkentLayout = {
     // Divisor to compute edge forces
     edgeElasticity: 1.5,
     // Nesting factor (multiplier) to compute ideal edge length for inter-graph edges
-    nestingFactor: 0.1,
+    nestingFactor: 1.1,
     // Gravity force (constant)
-    gravity: 1.0,
+    gravity: 10.0,
     // Maximum number of iterations to perform
     numIter: 500,
     // Whether to tile disconnected nodes
     tile: true,
     // Type of layout animation. The option set is {'during', 'end', false}
-    animate: 'during',
+    animate: 'end',
     // Amount of vertical space to put between degree zero nodes during tiling (can also be a function)
     tilingPaddingVertical: 10,
     // Amount of horizontal space to put between degree zero nodes during tiling (can also be a function)
@@ -209,11 +209,98 @@ var cytoCoseBilkentLayout = {
     // Gravity range (constant) for compounds
     gravityRangeCompound: 1.5,
     // Gravity force (constant) for compounds
-    gravityCompound: 1.0,
+    gravityCompound: 10.0,
     // Gravity range (constant)
-    gravityRange: 3.8,
+    gravityRange: 5.0,
     // Initial cooling factor for incremental layout
-    initialEnergyOnIncremental: 0.5
+    initialEnergyOnIncremental: 0.1
+  };
+
+  var cytoCoseLayout = {
+    name: 'cose',
+  
+    // Called on `layoutready`
+    ready: function(){},
+  
+    // Called on `layoutstop`
+    stop: function(){},
+    
+    // Whether to animate while running the layout
+    // true : Animate continuously as the layout is running
+    // false : Just show the end result
+    // 'end' : Animate with the end result, from the initial positions to the end positions
+    animate: true,
+  
+    // Easing of the animation for animate:'end'
+    animationEasing: undefined,
+  
+    // The duration of the animation for animate:'end'
+    animationDuration: undefined,
+  
+    // A function that determines whether the node should be animated
+    // All nodes animated by default on animate enabled
+    // Non-animated nodes are positioned immediately when the layout starts
+    animateFilter: function ( node, i ){ return true; },
+  
+  
+    // The layout animates only after this many milliseconds for animate:true
+    // (prevents flashing on fast runs)
+    animationThreshold: 250,
+  
+    // Number of iterations between consecutive screen positions update
+    // (0 -> only updated on the end)
+    refresh: 20,
+  
+    // Whether to fit the network view after when done
+    fit: true,
+  
+    // Padding on fit
+    padding: 30,
+  
+    // Constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+    boundingBox: undefined,
+  
+    // Excludes the label when calculating node bounding boxes for the layout algorithm
+    nodeDimensionsIncludeLabels: false,
+  
+    // Randomize the initial positions of the nodes (true) or use existing positions (false)
+    randomize: true,
+  
+    // Extra spacing between components in non-compound graphs
+    componentSpacing: 40,
+  
+    // Node repulsion (non overlapping) multiplier
+    nodeRepulsion: function( node ){ return 10048; },
+  
+    // Node repulsion (overlapping) multiplier
+    nodeOverlap: 100,
+  
+    // Ideal edge (non nested) length
+    idealEdgeLength: function( edge ){ return 16; },
+  
+    // Divisor to compute edge forces
+    edgeElasticity: function( edge ){ return 100; },
+  
+    // Nesting factor (multiplier) to compute ideal edge length for nested edges
+    nestingFactor: 1.2,
+  
+    // Gravity force (constant)
+    gravity: -3,
+  
+    // Maximum number of iterations to perform
+    numIter: 1000,
+  
+    // Initial temperature (maximum node displacement)
+    initialTemp: 1000,
+  
+    // Cooling factor (how the temperature is reduced between consecutive iterations
+    coolingFactor: 0.7,
+  
+    // Lower temperature threshold (below this point the layout will end)
+    minTemp: 1.0,
+  
+    // Pass a reference to weaver to use threads for calculations
+    weaver: false
   };
 
 var cytoArborLayout = {
@@ -453,8 +540,8 @@ class CytoGraph extends React.Component {
             container: document.getElementById('cy'),
             elements: this.state.graph,
             style: cytoStyle,
-            layout: cytoCoseBilkentLayout,
-            minZoom: 0.5,
+            layout: cytoCoseLayout,
+            minZoom: 0.1,
             maxZoom: 1.5,
             zoomingEnabled: true,
             userZoomingEnabled: true,
