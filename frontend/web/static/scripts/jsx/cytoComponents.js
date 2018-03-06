94d71119ed45ed8b2b73cefc35410320d0d85e05
sfbrunner <sibrunne@gmail.com>
2018-03-06 19:12:45 +0100

    Activated Euler layout

--- frontend/web/static/scripts/jsx/cytoComponents.js
+++ frontend/web/static/scripts/jsx/cytoComponents.js
@@ -33,8 +33,9 @@ cycola(cytoscape);
 cyforcelayout( cytoscape );
 //var cycola = require('cytoscape-cola');
 import coseBilkent from 'cytoscape-cose-bilkent';
+import euler from 'cytoscape-euler';
 
-cytoscape.use( coseBilkent );
+cytoscape.use( euler );
 
 //var cyarbor = require('cytoscape-arbor');
 //var arbor = require('arbor');
@@ -303,6 +304,100 @@ var cytoCoseBilkentLayout = {
     weaver: false
   };
 
+  var cytoEuler = {
+    name: 'euler',
+  
+    // The ideal length of a spring
+    // - This acts as a hint for the edge length
+    // - The edge length can be longer or shorter if the forces are set to extreme values
+    springLength: edge => 100,
+  
+    // Hooke's law coefficient
+    // - The value ranges on [0, 1]
+    // - Lower values give looser springs
+    // - Higher values give tighter springs
+    springCoeff: edge => 0.0008,
+  
+    // The mass of the node in the physics simulation
+    // - The mass affects the gravity node repulsion/attraction
+    mass: node => 5,
+  
+    // Coulomb's law coefficient
+    // - Makes the nodes repel each other for negative values
+    // - Makes the nodes attract each other for positive values
+    gravity: -1.2,
+  
+    // A force that pulls nodes towards the origin (0, 0)
+    // Higher values keep the components less spread out
+    pull: 0.01,
+  
+    // Theta coefficient from Barnes-Hut simulation
+    // - Value ranges on [0, 1]
+    // - Performance is better with smaller values
+    // - Very small values may not create enough force to give a good result
+    theta: 0.8,
+  
+    // Friction / drag coefficient to make the system stabilise over time
+    dragCoeff: 0.02,
+  
+    // When the total of the squared position deltas is less than this value, the simulation ends
+    movementThreshold: 0.01,
+  
+    // The amount of time passed per tick
+    // - Larger values result in faster runtimes but might spread things out too far
+    // - Smaller values produce more accurate results
+    timeStep: 20,
+  
+    // The number of ticks per frame for animate:true
+    // - A larger value reduces rendering cost but can be jerky
+    // - A smaller value increases rendering cost but is smoother
+    refresh: 20,
+  
+    // Whether to animate the layout
+    // - true : Animate while the layout is running
+    // - false : Just show the end result
+    // - 'end' : Animate directly to the end result
+    animate: false,
+  
+    // Animation duration used for animate:'end'
+    animationDuration: undefined,
+  
+    // Easing for animate:'end'
+    animationEasing: undefined,
+  
+    // Maximum iterations and time (in ms) before the layout will bail out
+    // - A large value may allow for a better result
+    // - A small value may make the layout end prematurely
+    // - The layout may stop before this if it has settled
+    maxIterations: 10000,
+    maxSimulationTime: 1000,
+  
+    // Prevent the user grabbing nodes during the layout (usually with animate:true)
+    ungrabifyWhileSimulating: false,
+  
+    // Whether to fit the viewport to the repositioned graph
+    // true : Fits at end of layout for animate:false or animate:'end'; fits on each frame for animate:true
+    fit: true,
+  
+    // Padding in rendered co-ordinates around the layout
+    padding: 30,
+  
+    // Constrain layout bounds with one of
+    // - { x1, y1, x2, y2 }
+    // - { x1, y1, w, h }
+    // - undefined / null : Unconstrained
+    boundingBox: undefined,
+  
+    // Layout event callbacks; equivalent to `layout.one('layoutready', callback)` for example
+    ready: function(){}, // on layoutready
+    stop: function(){}, // on layoutstop
+  
+    // Whether to randomize the initial positions of the nodes
+    // true : Use random positions within the bounding box
+    // false : Use the current node positions as the initial positions
+    randomize: true
+  };
+
 var cytoArborLayout = {
     animate: true, // whether to show the layout as it's running
     maxSimulationTime: 1000, // max length in ms to run the layout
@@ -540,7 +635,7 @@ class CytoGraph extends React.Component {
             container: document.getElementById('cy'),
             elements: this.state.graph,
             style: cytoStyle,
-            layout: cytoCoseLayout,
+            layout: cytoEuler,
             minZoom: 0.1,
             maxZoom: 1.5,
             zoomingEnabled: true,
