96d04c967283f372db5cca1a6b6d4afc1918f377
rmishr <ravi.mishra@gmx.net>
2018-03-17 14:25:42 +0100

    initial commit.

---
+++ frontend/web/webpack.common.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './static/scripts/jsx/routehandler.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Production'
+        })
+    ],
+    output: {
+        path: __dirname + "/static/scripts/js", 
+        filename: "routehandler.js"
+    }
+};
