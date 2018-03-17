e3f2e6328cb712aae691571efbfacf2b4d1b81ca
rmishr <ravi.mishra@gmx.net>
2018-03-17 15:03:01 +0100

    Added prod and dev webpack configs.

--- frontend/web/webpack.common.js
+++ frontend/web/webpack.common.js
@@ -7,11 +7,24 @@ module.exports = {
         app: './static/scripts/jsx/routehandler.js'
     },
     plugins: [
-        new CleanWebpackPlugin(['dist']),
+        new CleanWebpackPlugin(['./static/scripts/js']),
         new HtmlWebpackPlugin({
             title: 'Production'
         })
     ],
+    module: {
+        loaders: [
+          {
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader?cacheDirectory=true',
+            query: {
+              presets: ['react', 'es2015', 'stage-0'],
+              plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+            }
+          }
+        ]
+      },
     output: {
         path: __dirname + "/static/scripts/js", 
         filename: "routehandler.js"
