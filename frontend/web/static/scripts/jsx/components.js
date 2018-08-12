7768654a6b495a96e13db6bb44060f4bb416dcef
sfbrunner <sibrunne@gmail.com>
2018-08-12 18:34:21 +0100

    Added favicons

--- frontend/web/static/scripts/jsx/components.js
+++ frontend/web/static/scripts/jsx/components.js
@@ -68,6 +68,7 @@ class ErrorBoundary extends React.Component {
       this.setState({ hasError: true });
       // You can also log the error to an error reporting service
       // logErrorToMyService(error, info);
+      console.log(error, info)
     }
   
     render() {
